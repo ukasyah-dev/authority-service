37f0aa55827c44d140f9e89fb9149ec49015c36d
Ukasyah <hi.ukasyah@gmail.com>
2024-07-19 21:02:31 +0700

    fix mising grpc server

--- controller/team/create_team.go
+++ controller/team/create_team.go
@@ -3,8 +3,10 @@ package team
 import (
 	"context"
 
+	"github.com/ukasyah-dev/authority-service/controller/team_member"
 	"github.com/ukasyah-dev/authority-service/db"
 	"github.com/ukasyah-dev/authority-service/model"
+	"github.com/ukasyah-dev/common/constant"
 	"github.com/ukasyah-dev/common/errors"
 	"github.com/ukasyah-dev/common/id"
 	"github.com/ukasyah-dev/common/log"
@@ -24,6 +26,19 @@ func CreateTeam(ctx context.Context, req *model.CreateTeamRequest) (*model.Team,
 	if err := db.DB.Create(team).Error; err != nil {
 		log.Errorf("Failed to create team: %s", err)
 		return nil, errors.Internal()
+	}
+
+	userID, _ := ctx.Value(constant.UserID).(string)
+
+	if userID != "" {
+		_, err := team_member.CreateTeamMember(ctx, &model.CreateTeamMemberRequest{
+			RoleID: "admin",
+			TeamID: team.ID,
+			UserID: userID,
+		})
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return team, nil
--- controller/team/get_teams.go
+++ controller/team/get_teams.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ukasyah-dev/authority-service/db"
 	"github.com/ukasyah-dev/authority-service/model"
+	"github.com/ukasyah-dev/common/constant"
 	"github.com/ukasyah-dev/common/paginator"
 	"github.com/ukasyah-dev/common/validator"
 )
@@ -15,6 +16,12 @@ func GetTeams(ctx context.Context, req *model.GetTeamsRequest) (*model.GetTeamsR
 	}
 
 	tx := db.DB.WithContext(ctx).Model(&model.Team{})
+
+	userID, _ := ctx.Value(constant.UserID).(string)
+
+	if userID != "" {
+		tx = tx.Joins("INNER JOIN team_members tm ON tm.team_id = teams.id AND tm.user_id = ?", userID)
+	}
 
 	data, pagination, err := paginator.Paginate[model.Team](tx, &req.PaginationRequest)
 	if err != nil {
