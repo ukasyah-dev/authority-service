7983b992bfd006afe7831c1b451ed6ce773f4658
Ukasyah <hi.ukasyah@gmail.com>
2024-07-19 21:05:16 +0700

    bugfix (#2)
    * wip
    
    * add role controller
    
    * crud role
    
    * wip permission
    
    * some updates
    
    * update basic functionality
    
    * fix mising grpc server

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"os"
 
 	"github.com/appleboy/graceful"
 	"github.com/caitlinelfring/go-env-default"
 	"github.com/ukasyah-dev/authority-service/db"
 	"github.com/ukasyah-dev/authority-service/rest"
+	"github.com/ukasyah-dev/authority-service/rpc"
 	"github.com/ukasyah-dev/common/amqp"
+	"github.com/ukasyah-dev/common/log"
 	identityModel "github.com/ukasyah-dev/identity-service/model"
 	pb "github.com/ukasyah-dev/pb/authority"
 	"google.golang.org/grpc"
@@ -24,8 +28,18 @@ func init() {
 }
 
 func main() {
+	grpcPort := env.GetIntDefault("GRPC_PORT", 4000)
+
+	// Create authority server
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		panic(err)
+	}
+	authorityServer := grpc.NewServer()
+	pb.RegisterAuthorityServer(authorityServer, &rpc.Server{})
+
 	// Create authority client
-	addr := "localhost:" + env.GetDefault("GRPC_PORT", "4000")
+	addr := fmt.Sprintf("localhost:%d", grpcPort)
 	authorityClientConn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
@@ -46,6 +60,16 @@ func main() {
 
 	m.AddShutdownJob(func() error {
 		return s.Shutdown()
+	})
+
+	m.AddRunningJob(func(ctx context.Context) error {
+		log.Infof("gRPC server is listening at %s", lis.Addr())
+		return authorityServer.Serve(lis)
+	})
+
+	m.AddShutdownJob(func() error {
+		authorityServer.GracefulStop()
+		return nil
 	})
 
 	m.AddShutdownJob(func() error {
